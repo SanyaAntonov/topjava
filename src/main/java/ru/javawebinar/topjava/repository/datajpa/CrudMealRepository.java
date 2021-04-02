34a2b5f3a8db7afd702cac1bae58d1be87e6b766
Alex Antonov <kritik145@gmail.com>
2021-04-02 13:24:46 +0300

    6_01_HW5_data_jpa

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,12 +1,26 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
-public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
-    Meal save(Meal meal, int userId);
+import java.time.LocalDateTime;
+import java.util.List;
 
-    @Query("DELETE FROM Meal m WHERE m.id=?1 AND m.user.id=?2")
-    int delete(int id, int userId);
+@Transactional(readOnly = true)
+public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId")
+    int delete(@Param("id") int id, @Param("userId") int userId);
+
+    @Query("SELECT m FROM Meal m WHERE m.user.id=:userId ORDER BY m.dateTime DESC")
+    List<Meal> getAll(@Param("userId") int userId);
+
+    @Query("SELECT m from Meal m WHERE m.user.id=:userId AND m.dateTime >= :startDate AND m.dateTime < :endDate ORDER BY m.dateTime DESC")
+    List<Meal> getBetweenHalfOpen(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, @Param("userId") int userId);
 }
